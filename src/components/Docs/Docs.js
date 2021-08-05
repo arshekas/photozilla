3d1d83730985c0dded5bf78ed805443974ed2c83
Arshpreet Singh <asingh206@mylangara.ca>
2021-08-04 20:30:09 -0700

    Done

--- src/components/Docs/Docs.js
+++ src/components/Docs/Docs.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Docs() {
     return (
         <div>
-            Docs
+            <ul>
+                <li>Hello</li>
+            </ul>
         </div>
     )
 }
